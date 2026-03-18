412e019c6f0e1f3f3fd19f275bf97dac9a29e8b7
Kriish Patel <kriishpatel27@gmail.com>
2026-03-18 16:59:13 -0500

    added reposniveness and some UI changes(icons) + colors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,10 @@
 import './index.css'
 import {Routes, Route} from 'react-router-dom'
-
-
 import Homepage from './pages/Homepage'
 import Radar from './pages/Radar'
 
 
 function App() {
-
-  
 
   return (
      <Routes>
